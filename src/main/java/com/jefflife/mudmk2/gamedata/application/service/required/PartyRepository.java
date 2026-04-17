bd1aef581aa9efe7b9c7c8c5205836212989b622
ch3224bin <subrime1987@gmail.com>
2026-04-17 21:47:25 +0900

    fix: PartyRepository 제네릭 타입 Long → UUID 수정

--- src/main/java/com/jefflife/mudmk2/gamedata/application/service/required/PartyRepository.java
+++ src/main/java/com/jefflife/mudmk2/gamedata/application/service/required/PartyRepository.java
@@ -2,6 +2,7 @@ package com.jefflife.mudmk2.gamedata.application.service.required;
 
 import com.jefflife.mudmk2.gamedata.application.domain.model.party.Party;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
 
-public interface PartyRepository extends JpaRepository<Party, Long> {
+public interface PartyRepository extends JpaRepository<Party, UUID> {
 }
