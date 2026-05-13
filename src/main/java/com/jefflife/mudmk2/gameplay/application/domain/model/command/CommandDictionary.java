658df7d8aad9fb6f7db5ddf4958f5fd51ac5ef39
ch3224bin <ahrocksil@gmail.com>
2026-05-13 09:18:32 +0900

    feat(command): 커맨드 사전에 별칭 추가 및 확장
    - TAKE 커맨드에 새 별칭 "집" 추가
    - DROP 커맨드에 새 별칭 "버" 추가
    - 사용자 입력 처리 시 편의성 향상 목적

--- src/main/java/com/jefflife/mudmk2/gameplay/application/domain/model/command/CommandDictionary.java
+++ src/main/java/com/jefflife/mudmk2/gameplay/application/domain/model/command/CommandDictionary.java
@@ -4,8 +4,8 @@ import java.util.Set;
 
 public enum CommandDictionary {
     ATTACK("공격", "때려", "공"),
-    TAKE("줍다", "주워", "집어"),
-    DROP("버리다", "버려", "놓다"),
+    TAKE("줍다", "주워", "집어", "집"),
+    DROP("버리다", "버려", "놓다", "버"),
     INVENTORY("소지품", "가방", "인벤");
 
     private final Set<String> aliases;
