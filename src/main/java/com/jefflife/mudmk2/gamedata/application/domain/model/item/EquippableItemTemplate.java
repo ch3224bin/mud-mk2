d35060c5dcd83332688b5f8b3a598db4dcfaa571
ch3224bin <ahrocksil@gmail.com>
2026-05-16 00:28:05 +0900

    docs(equip): EquippableItemTemplate 설계 의도 Javadoc 추가

--- src/main/java/com/jefflife/mudmk2/gamedata/application/domain/model/item/EquippableItemTemplate.java
+++ src/main/java/com/jefflife/mudmk2/gamedata/application/domain/model/item/EquippableItemTemplate.java
@@ -2,6 +2,12 @@ package com.jefflife.mudmk2.gamedata.application.domain.model.item;
 
 import java.util.List;
 
+/**
+ * Marker interface for ItemTemplates that can be equipped.
+ * Equipped templates expose their stat modifiers; target slot resolution is delegated
+ * to the equip service because RING accessories choose between RING_LEFT/RING_RIGHT
+ * based on current EquippedItems state.
+ */
 public interface EquippableItemTemplate {
     List<StatModifier> getStatModifiers();
 }
